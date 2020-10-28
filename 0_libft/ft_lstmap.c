9e926b9e104762f58e71bc736a6c8a1debae0737
Hangyeol Kim <hangkim@c5r6s7.42seoul.kr>
2020-10-28 21:28:09 +0900

    makefile

--- 0_libft/ft_lstmap.c
+++ 0_libft/ft_lstmap.c
@@ -6,11 +6,30 @@
 /*   By: hangkim <hangkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 18:37:16 by hangkim           #+#    #+#             */
-/*   Updated: 2020/10/23 18:40:00 by hangkim          ###   ########.fr       */
+/*   Updated: 2020/10/28 17:50:54 by hangkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{}
+{
+    t_list  *res;
+    t_list  *tmp;
+
+    if (!(res = ft_lstnew(f(lst->content)))
+        return (0);
+    tmp = res;
+    lst = lst->next;
+    while (lst)
+    {
+        if (!(tmp->next = ft_lstnew(f(lst->next))))
+        {
+            ft_lstclear(&res, del);
+            return (0);
+        }
+        tmp = tmp->next;
+        lst = lst->next;
+    }
+    return (res);
+}
